742aff3ebc9004a9edcd60ebfc7ec50fc6c69a83
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-07-05 23:20:36 +0100

    remove lol

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,110 +1,127 @@
-import { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import NavHome from "./Components/NavHome/NavHome";
-import GetStart from "./Components/GetStart/GetStart";
-import About from "./Components/About/About";
-import ContactUs from "./Components/ContactUs/ContactUs";
-import Register from "./Components/Register/Pages/Auth/Register";
-import Forgot from "./Components/Register/Pages/Auth/Forgot";
-import Reset from "./Components/Register/Pages/Auth/Reset";
-import Login from "./Components/Register/Pages/Auth/Login";
-import LoginWithCode from "./Components/Register/Pages/Auth/LoginWithCode";
-import Profile from "./Components/Register/Pages/Profile/Profile";
-import Verify from "./Components/Register/Pages/Auth/Verify";
-import PageMenu from "./Components/Register/Component/PageMenu/PageMenu";
-import axios from "axios";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  getLoginStatus,
-  getUser,
-  selectIsLoggedIn,
-  selectUser,
-} from "./Components/Register/Redux/Features/Auth/authSlice";
-import ChangePassword from "./Components/Register/ChangePassWord/ChangePass";
-import UserList from "./Components/Register/Pages/UserList/UserList";
-import Navbar from "./Components/NavHome/Navbar/Navbar";
-import Aspect1 from "./Components/NavHome/Home/Aspect1/Aspect1";
-import Sponsors from "./Components/NavHome/Home/Aspect1/Sponsors/Sponsors";
-import Aspect2 from "./Components/NavHome/Home/Aspect2/Aspect2";
-import UseCase from "./Components/NavHome/Home/UseCase/UseCase";
-import Customers from "./Components/NavHome/Home/Customers/Customers";
-import WhyUs from "./Components/NavHome/Home/WhyUs/WhyUs";
-import ContactMe from "./Components/NavHome/Home/ContactMe/ContactMe";
-import Footer from "./Components/NavHome/Home/Footer/Footer";
-import History from "./Components/GetStart/History/History";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import Loader from "./Loader/Loader";
-import AddDashBoard from "./Components/GetStart/Work/AddMoney/CompileAdd";
-import SendMoneyCompnent from "./Components/GetStart/Work/SendMoney/SendMoneyCompnent";
-import Success from "./Succes-Page/Success";
-import Home from "./Components/NavHome/Home/Home";
-// import TierOne from "./Components/GetStart/TierOne/TierOne";
-// import AuthContextProvider from "./Store/AuthContextProvider";
+// import { useState, useEffect } from "react";
+// import { Route, Routes } from "react-router-dom";
+// import "./App.css";
+// import NavHome from "./Components/NavHome/NavHome";
+// import GetStart from "./Components/GetStart/GetStart";
+// import About from "./Components/About/About";
+// import ContactUs from "./Components/ContactUs/ContactUs";
+// import Register from "./Components/Register/Pages/Auth/Register";
+// import Forgot from "./Components/Register/Pages/Auth/Forgot";
+// import Reset from "./Components/Register/Pages/Auth/Reset";
+// import Login from "./Components/Register/Pages/Auth/Login";
+// import LoginWithCode from "./Components/Register/Pages/Auth/LoginWithCode";
+// import Profile from "./Components/Register/Pages/Profile/Profile";
+// import Verify from "./Components/Register/Pages/Auth/Verify";
+// import PageMenu from "./Components/Register/Component/PageMenu/PageMenu";
+// import axios from "axios";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { useDispatch, useSelector } from "react-redux";
+// import {
+//   getLoginStatus,
+//   getUser,
+//   selectIsLoggedIn,
+//   selectUser,
+// } from "./Components/Register/Redux/Features/Auth/authSlice";
+// import ChangePassword from "./Components/Register/ChangePassWord/ChangePass";
+// import UserList from "./Components/Register/Pages/UserList/UserList";
+// import Navbar from "./Components/NavHome/Navbar/Navbar";
+// import Aspect1 from "./Components/NavHome/Home/Aspect1/Aspect1";
+// import Sponsors from "./Components/NavHome/Home/Aspect1/Sponsors/Sponsors";
+// import Aspect2 from "./Components/NavHome/Home/Aspect2/Aspect2";
+// import UseCase from "./Components/NavHome/Home/UseCase/UseCase";
+// import Customers from "./Components/NavHome/Home/Customers/Customers";
+// import WhyUs from "./Components/NavHome/Home/WhyUs/WhyUs";
+// import ContactMe from "./Components/NavHome/Home/ContactMe/ContactMe";
+// import Footer from "./Components/NavHome/Home/Footer/Footer";
+// import History from "./Components/GetStart/History/History";
+// import { GoogleOAuthProvider } from "@react-oauth/google";
+// import Loader from "./Loader/Loader";
+// import AddDashBoard from "./Components/GetStart/Work/AddMoney/CompileAdd";
+// import SendMoneyCompnent from "./Components/GetStart/Work/SendMoney/SendMoneyCompnent";
+// import Success from "./Succes-Page/Success";
+// import Home from "./Components/NavHome/Home/Home";
+// // import TierOne from "./Components/GetStart/TierOne/TierOne";
+// // import AuthContextProvider from "./Store/AuthContextProvider";
 
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
-function App() {
-  const dispatch = useDispatch();
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const user = useSelector(selectUser);
-  useEffect(() => {
-    dispatch(getLoginStatus());
+// function App() {
+//   const dispatch = useDispatch();
+//   const isLoggedIn = useSelector(selectIsLoggedIn);
+//   const user = useSelector(selectUser);
+//   useEffect(() => {
+//     dispatch(getLoginStatus());
 
-    if (isLoggedIn && user === null) {
-      dispatch(getUser());
-    }
-  }, [dispatch, isLoggedIn, user]);
-  const [loading, setLoading] = useState(true);
+//     if (isLoggedIn && user === null) {
+//       dispatch(getUser());
+//     }
+//   }, [dispatch, isLoggedIn, user]);
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setLoading(false);
+//     }, 2000);
+//   }, []);
 
+//   return (
+//     <>
+//       <div className="app-app">
+      // <Navbar />
+//         <ToastContainer />
+//         <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+//           {loading ? (
+//             <Loader />
+//           ) : (
+//             <Routes>
+//               {/* <Route path="/" element={<Home />} /> */}
+//               <Route path="/get-start/home" element={<GetStart />} />
+//               <Route path="/get-start/home/add-money" element={<AddDashBoard/> } />
+//               <Route path="/get-start/home/send-money" element={<SendMoneyCompnent /> } />
+//               <Route path="/about" element={<About />} />
+//               <Route path="/contact-us" element={<ContactUs />} />
+//               <Route path="/login" element={<Login />} />
+//               <Route path="/success-page" element={<Success /> } />
+//               <Route path="/get-start/profile" element={<Profile />} />
+//               <Route
+//                 path="/get-start/transaction-history"
+//                 element={<History />}
+//               />
+//               <Route path="/register" element={<Register />} />
+//               <Route path="/forgot" element={<Forgot />} />
+//               <Route path="/forgot" element={<Forgot />} />
+//               <Route path="/verify/:verificationToken" element={<Verify />} />
+//               <Route path="/resetPassword/:resetToken" element={<Reset />} />
+//               <Route path="/changepassword" element={<ChangePassword />} />
+//               <Route path="/user" element={<UserList />} />
+//               <Route path="/loginwithcode/:email" element={<LoginWithCode />} />
+//             </Routes>
+//           )}
+//         </GoogleOAuthProvider>
+//         {/* <Card/> */}
+//       </div>
+//     </>
+//     // AuthContextProvider
+//     // AuthContextProvider
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+import React from 'react'
+import Navbar from './Components/NavHome/Navbar/Navbar'
+
+const App = () => {
   return (
-    <>
-      <div className="app-app">
-      <Navbar />
-        <ToastContainer />
-        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-          {loading ? (
-            <Loader />
-          ) : (
-            <Routes>
-              {/* <Route path="/" element={<Home />} /> */}
-              <Route path="/get-start/home" element={<GetStart />} />
-              <Route path="/get-start/home/add-money" element={<AddDashBoard/> } />
-              <Route path="/get-start/home/send-money" element={<SendMoneyCompnent /> } />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact-us" element={<ContactUs />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/success-page" element={<Success /> } />
-              <Route path="/get-start/profile" element={<Profile />} />
-              <Route
-                path="/get-start/transaction-history"
-                element={<History />}
-              />
-              <Route path="/register" element={<Register />} />
-              <Route path="/forgot" element={<Forgot />} />
-              <Route path="/forgot" element={<Forgot />} />
-              <Route path="/verify/:verificationToken" element={<Verify />} />
-              <Route path="/resetPassword/:resetToken" element={<Reset />} />
-              <Route path="/changepassword" element={<ChangePassword />} />
-              <Route path="/user" element={<UserList />} />
-              <Route path="/loginwithcode/:email" element={<LoginWithCode />} />
-            </Routes>
-          )}
-        </GoogleOAuthProvider>
-        {/* <Card/> */}
-      </div>
-    </>
-    // AuthContextProvider
-    // AuthContextProvider
-  );
+    <div>
+         <Navbar />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- front-end/src/Components/NavHome/Navbar/Navbar.jsx
+++ front-end/src/Components/NavHome/Navbar/Navbar.jsx
@@ -1,138 +1,30 @@
-// import { BsArrowRightShort } from "react-icons/bs";
-// import { NavLink } from "react-router-dom";
-// import "./Navbar.css";
-// import { useContext, useState } from "react";
-// import { ThemeContext } from "../../../ThemeContext";
-
-// const Navbar = () => {
-//   const [active, setActive] = useState("coin-base-links_m");
-//   const [toggleIcon, setToggleIcon] = useState("navbar_toggle");
-//   const navToggle = () => {
-//     active === "coin-base-links_m"
-//       ? setActive("coin-base-links_m nav_active")
-//       : setActive("coin-base-links_m");
-
-//     //Toggler Icon
-
-//     toggleIcon === "navbar_toggle"
-//       ? setToggleIcon("navbar_toggle toggle")
-//       : setToggleIcon("navbar_toggle");
-//   };
-
-//   const { toggleTheme } = useContext(ThemeContext);
-
-
-//   return (
-//     <nav className="navbar">
-//       <div>
-//         <NavLink className="nyb-wallet-b" to="/">
-//           shaftCoin
-//         </NavLink>
-//       </div>
-//       <div className={active}>
-//         <NavLink to="/dashboard">
-//           <span>Dashboard</span>
-//           <span className="media_icon">
-//             <BsArrowRightShort size={20} />
-//           </span>
-//         </NavLink>
-//         <NavLink target="_blank" to="https://www.coinbase.com/learn">
-//           <span>Learn</span>
-//           <span className="media_icon">
-//             <BsArrowRightShort size={20} />
-//           </span>
-//         </NavLink>
-//         <NavLink target="_blank" to="https://www.coinbase.com/products">
-//           <span>Individuals</span>
-//           <span className="media_icon">
-//             <BsArrowRightShort size={20} />
-//           </span>
-//         </NavLink>
-//         <NavLink target="_blank" to="https://www.coinbase.com/exchange">
-//           <span>Businesses</span>
-//           <span className="media_icon">
-//             <BsArrowRightShort size={20} />
-//           </span>
-//         </NavLink>
-//         <NavLink
-//           target="_blank"
-//           to="https://www.coinbase.com/developer-platform"
-//         >
-//           <span>Developers</span>
-//           <span className="media_icon">
-//             <BsArrowRightShort size={20} />
-//           </span>
-//         </NavLink>
-//         <NavLink target="_blank" to="https://www.coinbase.com/about">
-//           <span>Company</span>
-//           <span className="media_icon">
-//             <BsArrowRightShort size={20} />
-//           </span>
-//         </NavLink>
-
-//         <div className="sign-options">
-//         <button onClick={toggleTheme}>Toggle Theme</button>
-//           <button className="sign-up">Sign Up</button>
-//         </div>
-//       </div>
-//       <div className="sign-option">
-//       <button onClick={toggleTheme}>Toggle Theme</button>
-//         <button className="sign-up">Sign Up</button>
-//       </div>
-//       <div onClick={navToggle} className={toggleIcon}>
-//         <div className="line1"></div>
-//         <div className="line2"></div>
-//         <div className="line3"></div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import React from 'react'
+import React, { useContext } from "react";
+import shaftLogo from "../../../Assets/shaftCoin.png";
+import { NavLink } from "react-router-dom";
+import { ThemeContext } from "../../../ThemeContext";
 
 const Navbar = () => {
+  const { toggleTheme } = useContext(ThemeContext);
   return (
     <div>
-      <div></div>
-      <div></div>
+      <div>
+        <div>
+          <img src={shaftLogo} alt="shaftCoin" />
+        </div>
+        <div>
+          <NavLink>About</NavLink>
+          <NavLink>Contact Us</NavLink>
+          <NavLink>Dashboard</NavLink>
+          <NavLink>Dashboard</NavLink>
+          <NavLink>Dashboard</NavLink>
+        </div>
+      </div>
+      <div>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <NavLink>Sign Up</NavLink>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
