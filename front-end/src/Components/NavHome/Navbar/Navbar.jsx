db61612f9b27fc113c0c5daacccbfe1e574d4658
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-07-09 23:45:52 +0100

    gpod day

--- front-end/src/Components/NavHome/Navbar/Navbar.jsx
+++ front-end/src/Components/NavHome/Navbar/Navbar.jsx
@@ -1,28 +1,32 @@
 import React, { useContext } from "react";
 import shaftLogo from "../../../Assets/shaftCoin.png";
+import { FiMoon } from "react-icons/fi";
+import { IoSunnyOutline } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
 import { ThemeContext } from "../../../ThemeContext";
 import './Navbar.css'
 
 const Navbar = () => {
-  const { toggleTheme } = useContext(ThemeContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <div className="navbarComponent">
       <div className="familyOneForNavbar">
         <div>
           <img src={shaftLogo} alt="shaftCoin" />
         </div>
-        <div>
+        <div className='NavbarItem'>
           <NavLink>About</NavLink>
           <NavLink>Contact Us</NavLink>
-          <NavLink>Dashboard</NavLink>
-          <NavLink>Dashboard</NavLink>
-          <NavLink>Dashboard</NavLink>
+          <NavLink>Dashboarsd</NavLink>
+          <NavLink>Dashbsoard</NavLink>
+          <NavLink>Dashbsoard</NavLink>
         </div>
       </div>
-      <div>
-        <button onClick={toggleTheme}>Toggle Theme</button>
-        <NavLink>Sign Up</NavLink>
+      <div className="signUpAspect">
+        <button onClick={toggleTheme}>
+        {theme === 'light' ? <FiMoon size={23}/> : <IoSunnyOutline size={23}/>}
+        </button>
+        <NavLink className='signUp'>Sign Up</NavLink>
       </div>
     </div>
   );
